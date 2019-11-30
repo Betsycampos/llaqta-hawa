fb03d9a9897e14f3dbbdc32e76acb06fafe16ec8
Betsycampos <cbetsydalia@gmail.com>
2019-11-30 16:23:52 -0500

    Status

---
+++ src/mostrar-nav.js
@@ -0,0 +1,11 @@
+
+
+//sconst   menuPrincipal = document.getElementById("");
+
+
+document.getElementById('mostrar-nav').addEventListener('click',mostrarMenu);
+
+
+    function mostrarMenu() {
+        var nav = document.getElementById('menuPrincipal');
+        nav.classList.toggle('mostrar')}
